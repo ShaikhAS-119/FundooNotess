27926b6b6a854028116fce5353a16b92243f52f7
Shaikh Abid <alishaikh4199@gmail.com>
2024-05-29 13:27:21 +0530

    [Abid] added environment variables to diffrent server in visual studio

--- ModelLayer/obj/Debug/netcoreapp3.1/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/netcoreapp3.1/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746b7ac907802e731be554d249c7a1e13acc6a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0277972024e4d561ef8b0f8220e73ead28550986")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
